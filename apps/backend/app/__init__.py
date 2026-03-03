409add4b061529bddf9bfd7236fbec6a7f21a39b
chetanr25 <chetan250204@gmail.com>
2026-03-03 15:53:06 +0530

    added backend URL in CORS

--- apps/backend/app/__init__.py
+++ apps/backend/app/__init__.py
@@ -75,7 +75,12 @@ def create_app() -> FastAPI:
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["https://collegepathfinder.vercel.app", "http://localhost:5173", "http://localhost:3000"],
+        allow_origins=[
+            "https://collegepathfinder.vercel.app", 
+            "http://localhost:5173", 
+            "http://localhost:3000",
+            "https://13.205.81.121"
+        ],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
